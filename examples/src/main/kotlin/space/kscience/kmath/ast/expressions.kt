78eb750988ad44d03090041fca14cc9b8ecf0105
Iaroslav Postovalov <postovalovya@gmail.com>
2022-02-05 04:27:10 +0700

    Add complete constant folding in kmath-ast by introducing TypedMst, some minor changes

--- examples/src/main/kotlin/space/kscience/kmath/ast/expressions.kt
+++ examples/src/main/kotlin/space/kscience/kmath/ast/expressions.kt
@@ -6,21 +6,11 @@
 package space.kscience.kmath.ast
 
 import space.kscience.kmath.asm.compileToExpression
-import space.kscience.kmath.expressions.MstExtendedField
-import space.kscience.kmath.expressions.Symbol.Companion.x
+import space.kscience.kmath.expressions.invoke
+import space.kscience.kmath.operations.Algebra
 import space.kscience.kmath.operations.DoubleField
-import space.kscience.kmath.operations.invoke
 
 fun main() {
-    val expr = MstExtendedField {
-        x * 2.0 + number(2.0) / x - number(16.0) + asinh(x) / sin(x)
-    }.compileToExpression(DoubleField)
-
-    val m = DoubleArray(expr.indexer.symbols.size)
-    val xIdx = expr.indexer.indexOf(x)
-
-    repeat(10000000) {
-        m[xIdx] = 1.0
-        expr(m)
-    }
+    val expr = "x+2".parseMath().compileToExpression(DoubleField as Algebra<Double>)
+    expr("x" to 42.0)
 }
